51dd32de6cc31864ddfac370ab0a14df123afe3f
JhoanSLC <JhoanSLC4@gmail.com>
2024-08-30 06:56:25 -0500

    :tada: inicio de proyecto :tada:

---
+++ src/main/java/com/ejercicio_practico/ejercicio_practico/domain/Producto.java
@@ -0,0 +1,43 @@
+package com.ejercicio_practico.ejercicio_practico.domain;
+
+
+public class Producto {
+    private String codigo;
+    private String nombre;
+    private double price;
+    private int stock;
+
+    public Producto(String codigo, String nombre, double price, int stock) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.price = price;
+        this.stock = stock;
+    }
+    
+    public String getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public int getStock() {
+        return stock;
+    }
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    
+}
---
+++ src/test/java/com/ejercicio_practico/ejercicio_practico/EjercicioPracticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicio_practico.ejercicio_practico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioPracticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
